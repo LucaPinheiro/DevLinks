c5a6f429242b78ac60fbc1c5d09012cc0e4f172c
LucaPinheiro <lucapgomes11@gmail.com>
2023-06-17 01:07:02 -0300

    add avatar using github api

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function toggleMode() {
 
   html.classList.toggle('light')
 }
+
+function setAvatar() {
+  fetch("https://api.github.com/users/lucapinheiro")
+    .then((response) => response.json())
+    .then((json) => {
+      const avatar = document.getElementById("avatar")
+      avatar.src = json.avatar_url
+    })
+}
+
+setAvatar()
+
